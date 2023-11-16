074a3344d753a4fa1254e540d379e99368df2bc2
matheus13f <matheus-goes13@hotmail.com>
2023-11-16 15:16:55 -0300

    Adicionado Tailwind variants

---
+++ src/components/Button.tsx
@@ -0,0 +1,26 @@
+import React, { ComponentProps } from 'react'
+import { tv, VariantProps } from 'tailwind-variants'
+
+const button = tv({
+  base: [
+    'rounded-lg px-4 py-2 text-sm font-semibold outline-none shadow-sm',
+    'focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-violet-500',
+    'active:opacity-80',
+  ],
+  variants: {
+    variant: {
+      primary: 'bg-violet-600 text-white hover:bg-violte-700',
+      outline: 'border border-zinc-300 text-zinc-700 hover:bg-zinc-50',
+    },
+  },
+  defaultVariants: {
+    variant: 'primary',
+  },
+})
+
+type ButtonProps = ComponentProps<'button'> & VariantProps<typeof button>
+
+export function Button({ variant, ...props }: ButtonProps) {
+  // um componente botao fechado neles mesmo, recebe o children automaticamente quando utilizado em outros lugares
+  return <button {...props} className={button({ variant })} />
+}
